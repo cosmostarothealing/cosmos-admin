ac3b84d0b96206c8d20b8c86e676beec58239e2f
darshilkantaria11 <darshilkantaria11@gmail.com>
2025-04-25 22:20:14 +0530

    bug fixed

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -137,7 +137,7 @@ export default function ProductDetailPage() {
               <option value="Tumble Stone">Tumble Stone</option>
               <option value="Raw Stone">Raw Stone</option>
               <option value="Reiki Stone">Reiki Stone</option>
-              <option value="Worry stone">Worry stone</option>
+              <option value="Worry Stone">Worry stone</option>
               <option value="Spell Candles">Spell Candles</option>
               <option value="Bracelets">Bracelets</option>
             </select>
